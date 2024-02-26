4e7065ee0a150ac640db208e73a5af225aa8c1b4
iAdso <iadso.ao@gmail.com>
2024-02-26 16:09:46 -0500

    actualizacion de fecha

--- ordenventa/forms.py
+++ ordenventa/forms.py
@@ -1,11 +1,28 @@
 from django import forms
 from .models import OrdenVenta, ItemOrdenVenta
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 
 class OrdenVentaForm(forms.ModelForm):
+    fecha = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'})
+    )
     class Meta:
         model = OrdenVenta
         fields = ["codigosap", "proyecto", "direccion_proyecto", "observacion", "fecha"]
+
+    def __init__(self, *args, **kwargs):
+        super(OrdenVentaForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            'codigosap',
+            'proyecto',
+            'direccion_proyecto',
+            'observacion',
+            'fecha',
+            Submit('submit', 'Guardar', css_class='btn btn-primary mt-2')
+        )
 
 
 class ItemOrdenVentaForm(forms.ModelForm):
